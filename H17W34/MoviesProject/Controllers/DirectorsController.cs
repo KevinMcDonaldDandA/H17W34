c9316a3efa8832595e2316770ef518a35463453f
Kevin McDonald <kevin.mcdonald@dundeeandangus.ac.uk>
2019-02-22 09:38:05 +0000

    Directors Controller Added with Index Action and some blank Views for commong CRUD operations

---
+++ H17W34/MoviesProject/Controllers/DirectorsController.cs
@@ -0,0 +1,38 @@
+﻿/*
+*  File	    DirectorsController.cs
+*  Author	Kevin McDonald, Dundee & Angus College, kevin.mcdonald@dundeeandangus.ac.uk
+*  Date     22nd February 2019
+*  Purpose	The Controller class for our Director Entity
+*  Notes    This class is responsible for the Actions that occur in response to URL Requests.
+*
+*       Copyright © Kevin McDonald 2019. All rights reserved.
+*/
+
+using MoviesProject.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MoviesProject.Controllers
+{
+    public class DirectorsController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Directors
+        public ActionResult Index()
+        {
+            var directors = db.Directors.ToList();
+            return View(directors);
+        }        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+//------------------------------- End of File -------------------------------​

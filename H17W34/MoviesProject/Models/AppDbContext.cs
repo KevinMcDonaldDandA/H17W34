0c25ef5161376d41bb09f648e4b3b3f16b69d786
Kevin McDonald <kevin.mcdonald@dundeeandangus.ac.uk>
2019-02-22 09:30:20 +0000

    Added Director Model

--- H17W34/MoviesProject/Models/AppDbContext.cs
+++ H17W34/MoviesProject/Models/AppDbContext.cs
@@ -1,14 +1,23 @@
+/*
+*  File	    Director.cs
+*  Author	Kevin McDonald, Dundee & Angus College, kevin.mcdonald@dundeeandangus.ac.uk
+*  Date     22nd February 2019
+*  Purpose	The Model class responsible for our Database Interaction
+*  Notes    Any new model you wish to include in the Database should be added here as a DbSet
+*
+*       Copyright © Kevin McDonald 2019. All rights reserved.
+*/
 namespace MoviesProject.Models
 {
-    using System;
     using System.Data.Entity;
-    using System.ComponentModel.DataAnnotations.Schema;
-    using System.Linq;
 
     public partial class AppDbContext : DbContext
     {
         public AppDbContext() : base("name=AppDbContext") { }
-        
+
+        public DbSet<Director> Directors { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder) { }
     }
 }
+//------------------------------- End of File -------------------------------?
---
+++ H17W34/MoviesProject/Models/Director.cs
@@ -0,0 +1,28 @@
+﻿/*
+*  File	    Director.cs
+*  Author	Kevin McDonald, Dundee & Angus College, kevin.mcdonald@dundeeandangus.ac.uk
+*  Date     22nd February 2019
+*  Purpose	The Model class for our Director Entity
+*  Notes    
+*
+*       Copyright © Kevin McDonald 2019. All rights reserved.
+*/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesProject.Models
+{
+    public class Director
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="A Firstname is Required for a Director")]
+        [StringLength(25)]
+        public string FName { get; set; }
+
+        [Required(ErrorMessage = "A Surname is Required for a Director")]
+        [StringLength(50)]
+        public string LName { get; set; }
+    }
+}
+//------------------------------- End of File -------------------------------​

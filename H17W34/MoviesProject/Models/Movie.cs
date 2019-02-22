8a82b69a7cb3c912605514ce91fde36baaeb9329
Kevin McDonald <kevin.mcdonald@dundeeandangus.ac.uk>
2019-02-22 09:57:11 +0000

    Model Added: Movie

---
+++ H17W34/MoviesProject/Models/Movie.cs
@@ -0,0 +1,33 @@
+﻿/*
+*  File	    Movie.cs
+*  Author	Kevin McDonald, Dundee & Angus College, kevin.mcdonald@dundeeandangus.ac.uk
+*  Date     22nd February 2019
+*  Purpose	The Model class for our Movie Entity
+*  Notes    
+*
+*       Copyright © Kevin McDonald 2019. All rights reserved.
+*/
+
+using System.ComponentModel.DataAnnotations;
+
+namespace MoviesProject.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        [Display(Name ="User Rating")]
+        public double UserRating { get; set; }
+
+        //  Director FK
+        public int DirectorId { get; set; }
+
+        //  Navigation Property
+        public Director Director { get; set; }
+
+    }
+}
+//------------------------------- End of File -------------------------------​

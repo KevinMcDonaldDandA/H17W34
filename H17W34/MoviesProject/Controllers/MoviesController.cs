1b83aa2109ec0a09e3992ef9b064ea23a3a55042
Kevin McDonald <kevin.mcdonald@dundeeandangus.ac.uk>
2019-02-22 10:02:18 +0000

    Movie Controller Added with some Empty Views

---
+++ H17W34/MoviesProject/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿/*
+*  File	    MoviesController.cs
+*  Author	Kevin McDonald, Dundee & Angus College, kevin.mcdonald@dundeeandangus.ac.uk
+*  Date     22nd February 2019
+*  Purpose	The Controller class for our Movie Entity
+*  Notes    This class is responsible for the Actions that occur in response to URL Requests.
+*
+*       Copyright © Kevin McDonald 2019. All rights reserved.
+*/
+
+using MoviesProject.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MoviesProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private AppDbContext db = new AppDbContext();
+
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = db.Movies.Include(m => m.Director);
+            return View(movies.ToList());
+        }        
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+//------------------------------- End of File -------------------------------​
